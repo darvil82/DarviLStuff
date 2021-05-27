6dcc304bc2aca9c608420c2f487dc3ed0c0aaf51
DarviL82 <davidlosantos89@gmail.com>
2021-05-28 01:45:32 +0200

    wtf...

--- pongtest2.py
+++ pongtest2.py
@@ -6,7 +6,11 @@ from time import sleep
 from os import get_terminal_size, system as runsys
 from random import choice, randrange, randint
 from textwrap import dedent
+from sys import argv
+from urllib import request
 import argparse
+
+
 
 
 
@@ -32,7 +36,7 @@ def terminalOpt(*args):
 
     out = ""
     for arg in args:
-        out += ESCcodes.get(arg)
+        if arg: out += ESCcodes.get(arg)
 
     print(out, end="")
 
@@ -48,15 +52,21 @@ randomColor = lambda: [randint(0,255), randint(0,255), randint(0,255)]
 def showMsg(**kwargs):
     """Display a message with a bit of color."""
     global isValid
+
     for key in kwargs:
         value = kwargs.get(key)
         if key == "error":
             prefix = "\x1b[91mE"
             msg = value
             isValid = False
+        elif key == "good":
+            prefix = "\x1b[92m√"
+            msg = value
+        
         if key == "type":
             prefix += f": {value}"
-    print(prefix + ":\x1b[0m", msg)
+        
+    print(f" {prefix}:\x1b[0m", msg)
 
 
 def capValue(value, max=float('inf'), min=float('-inf')):
@@ -67,6 +77,22 @@ def capValue(value, max=float('inf'), min=float('-inf')):
         return min
     else:
         return value
+
+
+def updateScript(filepath):
+    # filepath = path.abspath(filepath)
+    try:
+        with request.urlopen("https://raw.githubusercontent.com/DarviL82/DarviLStuff/master/pongtest2.py") as rawData:
+            with open(filepath, "wb") as file:
+                file.write(rawData.read())
+    except PermissionError:
+        showMsg(error=f"Unable to write on the file '{filepath}'.", type="Update")
+        return
+    except Exception:
+        showMsg(error="An error ocurred while downloading the file.", type="Update")
+        return
+    showMsg(good=f"Downloaded latest version as '{filepath}'.", type="Update")
+
 
 
 
@@ -95,20 +121,19 @@ def parseArgs() -> bool:
         description="A small python script to display moving lines in the terminal.",
         epilog=dedent(f"""
             Conditional actions:
-                To use conditional actions, supply them formatted like 'action,[...]'.
-                Available actions to use:
+                To use conditional actions with the condition parameters, supply them
+                formatted like 'action,[...]'. Available actions to use:
 
-                - duplicate:    Create a new line like the current line.
-                - destroy:      Destroy the current line.
-                - newColor:     Change the color of the current line.
-                - clear:        Clear all the pixels of the current line.
-                - clearAll:     Clear all the pixels of all the lines.
-                - longer:       Make the current line 1 pixel longer.
-                - shorter:      Make the current line 1 pixel shorter.
-                - stop:         Disable current line movement.
-                - continue:     Enable current line movement.
-                - newLine:      Create a new line.
-            
+                    - duplicate     Create a new line like the current line.
+                    - destroy       Destroy the current line.
+                    - newColor      Change the color of the current line.
+                    - clear         Clear all the pixels of the current line.
+                    - clearAll      Clear all the pixels of all the lines.
+                    - longer        Make the current line 1 pixel longer.
+                    - shorter       Make the current line 1 pixel shorter.
+                    - stop          Disable current line movement.
+                    - continue      Enable current line movement.
+                    - newLine       Create a new line.
 
             Written by DarviL (David Losantos). Version {prjVersion}.
             Repository available at: \x1b[4mhttps://github.com/L89David/DarviLStuff\x1b[24m"""),
@@ -119,12 +144,14 @@ def parseArgs() -> bool:
     argparser.add_argument("-l", "--lenght", help=dedent("""\
         Length of the line. Use '0' to make it infinite.
         Note: A value of 0 might not be supported with some
-        other options. Default value is 10"""), type=int, default=10)
+        other options. Default value is 10."""), type=int, default=10)
     argparser.add_argument("-p", "--pos", help=dedent("""\
         Start position for all the lines. Position values
-        formatted like 'PosX:PosY,[...]'. If multiple
-        Position values are supplied, a random one will be
-        selected when creating a new line."""), type=str)
+        formatted like 'PosX:PosY,[...]'. PosX and PosY can
+        have a value of 'center' to make the line appear in
+        the center of that axis. If multiple Position values
+        are supplied, a random one will be selected when
+        creating a new line."""), type=str)
     argparser.add_argument("-c", "--color", help=dedent("""\
         Color of the lines. RGB values formatted like
         'RED:GREEN:BLUE,[...]'. If multiple RGB values are
@@ -133,10 +160,13 @@ def parseArgs() -> bool:
     argparser.add_argument("-C", "--chars", help=dedent("""\
         Select the line character to display. Default is '█'.
         If more than one character is supplied, the character
-        will be picked randomly from the string.
+        will be picked randomly from the string."""), type=str, default="█")
+    argparser.add_argument("-i", "--ignore", help=dedent("""\
+        Don't collide with lines with the same color as the
+        current line.
 
 
-        """), type=str, default="█")
+        """), action="store_true")
     argparser.add_argument("--onBorderCollision", help=dedent("""\
         Conditionally perform a set of actions when the line
         collides with the terminal border.
@@ -168,28 +198,26 @@ def parseArgs() -> bool:
         screen. If double --debug is used, appends all the
         events to the log file './pt2.log'. It is recommended
         to use 'tail -f' to view the contents of the file."""), action="count")
+    argparser.add_argument("--update", help="Update the script with the latest version and exit.", action="store_true")
 
     args = argparser.parse_args()
 
 
     isValid = True
-    if args.number <= 0: showMsg(error="Number of lines cannot be 0 or below.")
-    if args.lenght > 500: showMsg(error="Length cannot exceed 500.")
-    if args.max <= 0: showMsg(error="Number of max lines cannot be 0 or below.")
-    if len(args.chars) <= 0: showMsg(error="Specified invalid character/s.")
-
+    if args.number <= 0: showMsg(error="Number of lines cannot be 0 or below.", type="Number")
+    if args.lenght > 500: showMsg(error="Length cannot exceed 500.", type="Lenght")
+    if args.max <= 0: showMsg(error="Number of max lines cannot be 0 or below.", type="Max")
+    if len(args.chars) <= 0: showMsg(error="Specified invalid character/s.", type="Chars")
 
 
     def formatError(lst, index, delimiter=":") -> str:
         """
-        Return the word at the index provided colored in red as a string.
+        Return a colored string positioned at a given index inside a list.
         """
         item = lst[index]
         string = delimiter.join(str(x) for x in lst)
         itemPos = (string.index(item), string.index(item) + len(item))
-        return string[:itemPos[0]] + "\x1b[91m" + string[itemPos[0]:itemPos[1]] +  "\x1b[0m" + string[itemPos[1]:]
-        
-
+        return string[:itemPos[0]] + "\x1b[91m" + string[itemPos[0]:itemPos[1]] + "\x1b[0m" + string[itemPos[1]:]
 
 
     if args.pos:
@@ -203,11 +231,9 @@ def parseArgs() -> bool:
                     try:
                         posSplitted[valueIndex] = capValue(int(posvalue), windowSize[valueIndex], 2)
                     except ValueError:
-                        showMsg(error=f"Value '{posvalue}' at position {valueIndex + 1} is not an intenger. ({formatError(posSplitted, valueIndex)})", type="Position")
+                        showMsg(error=f"Value '{posvalue}' is not an intenger. ({formatError(posSplitted, valueIndex)})", type="Position")
                         break
-            else:
-                showMsg(error=f"Values X and Y are required (2), but {len(posSplitted)} value/s were supplied ('" + "', '".join(posSplitted) + "').", type="Position")
-                break
+            else: showMsg(error=f"Values X and Y are required (2), but {len(posSplitted)} value/s were supplied ('" + ", ".join(posSplitted) + "').", type="Position")
             argPos.append(posSplitted)
         
         if isValid: setattr(ArgValues, "pos", argPos)
@@ -219,39 +245,48 @@ def parseArgs() -> bool:
             rgbSplitted = rgb.split(":")
             if len(rgbSplitted) == 3:
                 for rgbvalue in rgbSplitted:
+                    valueIndex = rgbSplitted.index(rgbvalue)
                     try:
-                        if int(rgbvalue) not in range(0,256): showMsg(error=f"'{rgbvalue}' at position {rgbSplitted.index(rgbvalue) + 1} is not a value between '0' and '255'.")
+                        if int(rgbvalue) not in range(0,256):
+                            showMsg(error=f"'{rgbvalue}' is not a value between '0' and '255'. ({formatError(rgbSplitted, valueIndex)})")
+                            break
                     except ValueError:
-                        showMsg(error=f"Value '{rgbvalue}' at position {rgbSplitted.index(rgbvalue) + 1} is not an intenger. {formatError(rgbSplitted, rgbSplitted.index(rgbvalue))}", type="Color")
-            else:
-                showMsg(error=f"Values R, G and B are required (3), but {len(rgbSplitted)} value/s were supplied ('" + "', '".join(rgbSplitted) + "').", type="Color")
-                break
+                        showMsg(error=f"Value '{rgbvalue}' is not an intenger. ({formatError(rgbSplitted, valueIndex)})", type="Color")
+                        break
+            else: showMsg(error=f"Values R, G and B are required (3), but {len(rgbSplitted)} value/s were supplied ('" + ", ".join(rgbSplitted) + "').", type="Color")
             argColor.append(rgbSplitted)
         
         if isValid: setattr(ArgValues, "color", argColor)
     
 
 
-    conditions = {"onBorderCollision", "onMove", "onLineCollision", "onPathFree"}
-    condOptions = {"duplicate", "destroy", "newColor", "clear", "clearAll", "longer", "shorter", "stop", "continue", "newLine"}
 
     def parseConditions():
         # Go through every condition, and populate the ArgValues class with the variable and values.
+        conditions = {"onBorderCollision", "onMove", "onLineCollision", "onPathFree"}
+        condOptions = {"duplicate", "destroy", "newColor", "clear", "clearAll", "longer", "shorter", "stop", "continue", "newLine"}
+
         for cond in conditions:
             usrOpts = getattr(args, cond)
             if usrOpts:
+                usrOpts = usrOpts.split(",")
                 options = []
-                for opt in usrOpts.split(","):
-                    opt = opt.strip()
-                    if opt in condOptions:
-                        options.append(opt)
+                for opt in usrOpts:
+                    if opt.strip() in condOptions:
+                        options.append(opt.strip())
                     else:
-                        showMsg(error=f"'{opt}' is not a valid option.", type=cond)
+                        showMsg(error=f"'{opt}' is not a valid option. ({formatError(usrOpts, usrOpts.index(opt), ',')})", type=cond)
                         break
                 
                 if isValid: setattr(ArgValues, cond, options)
 
+
+
     parseConditions()
+    
+    if args.update:
+        updateScript(argv[0])
+        quit()
 
     return isValid
 
@@ -266,13 +301,13 @@ def parseArgs() -> bool:
 
 class Line:
     def __init__(self, **kwargs):
-        self._length = args.lenght + 1                                               # Length of the line.
-        self._color = randomColor()                                                  # Color of the line in RGB.
-        self._pos = [randrange(1, windowSize[0], 2), randrange(1, windowSize[1])]    # Position of the line.
-        self._state = [randint(0, 1), randint(0, 1)]                                 # Bools for controlling when to add or substract to the current pos.
-        self._posHistory = []                                                        # Position history of the line.
-        self._char = args.chars[randint(0,len(args.chars)-1)] * 2                    # Character to display as the line body.
-        self._doMove = True                                                          # Enable/Disable line movement.
+        self._length = args.lenght + 1                                                  # Length of the line.
+        self._color = randomColor()                                                     # Color of the line in RGB.
+        self._pos = [randrange(1, windowSize[0], 2), randrange(1, windowSize[1])]       # Position of the line.
+        self._state = [randint(0, 1), randint(0, 1)]                                    # Bools for controlling when to add or substract to the current pos.
+        self._posHistory = []                                                           # Position history of the line.
+        self._char = args.chars[randint(0,len(args.chars)-1)] * 2                       # Character to display as the line body.
+        self._doMove = True                                                             # Enable/Disable line movement.
 
         if ArgValues.pos: self._pos = choice(ArgValues.pos)
 
@@ -326,7 +361,8 @@ class Line:
             for obj in lines:
                 if obj is self.__class__: continue
                 if _nextPos in self._posHistory: continue
-                if obj._color == self._color: continue
+                if args.ignore:
+                    if obj._color == self._color: continue
                 if _nextPos in obj._posHistory:
                     if args.debug and args.debug >= 2: self.logmsg(f"Line collision at {self._pos}")
                     self.runOpts(ArgValues.onLineCollision)
@@ -380,32 +416,33 @@ class Line:
                 self._posHistory.pop(-1)
 
         print(
-            f"\x1b[{self._pos[1]};{self._pos[0]}f\x1b[38;2;{self._color[0]};{self._color[1]};{self._color[2]}m{self._char}",
+            f"\x1b[{self._pos[1]};{self._pos[0]}f\x1b[38;2;{self._color[0]};{self._color[1]};{self._color[2]}m{self._char}\x1b[H",
             end="",
             flush=True
         )
 
 
     def clearSegment(self, pos: list, ignoreSelf=False):
-        _brush = f"\x1b[{pos[1]};{pos[0]}f  "
+        _brush = "  "
 
         if pos in self._posHistory[0:-2] and ignoreSelf == False:
-            _brush = f"\x1b[{pos[1]};{pos[0]}f\x1b[38;2;{self._color[0]};{self._color[1]};{self._color[2]}m{self._char}"
+            _brush = f"\x1b[38;2;{self._color[0]};{self._color[1]};{self._color[2]}m{self._char}"
             if args.debug and args.debug >= 2: self.logmsg(f"Replaced self body at {pos}")
         else:
             for obj in lines:
                 if obj._posHistory is self._posHistory: continue
                 if pos in obj._posHistory:
-                    _brush = f"\x1b[{pos[1]};{pos[0]}f\x1b[38;2;{obj._color[0]};{obj._color[1]};{obj._color[2]}m{obj._char}"
+                    _brush = f"\x1b[38;2;{obj._color[0]};{obj._color[1]};{obj._color[2]}m{obj._char}"
                     if args.debug and args.debug >= 2: self.logmsg(f"Replaced \x1b[38;2;{obj._color[0]};{obj._color[1]};{obj._color[2]}m{obj._char}'s\x1b[0m body at {pos}")
                     break
 
-        print(_brush, end="", flush=True)
+        print(f"\x1b[{pos[1]};{pos[0]}f" + _brush, end="", flush=True)
     
 
     def runOpts(self, conditionList: list = None):
         # Run the selected actions listed on the supplied condition list.
         if conditionList:
+            if args.debug and args.debug >= 2: self.logmsg(f"Run action/s: " + ", ".join(conditionList) + ".")
             for opt in conditionList:
                 if opt == "duplicate" and len(lines) < args.max:
                     lines.append(Line(char=self._char, color=self._color))
@@ -423,7 +460,6 @@ class Line:
                         for pos in self._posHistory:
                             self.clearSegment(pos, True)
                         lines.remove(self)
-                        if args.debug and args.debug >= 2: self.logmsg("Killed self")
                 elif opt == "longer":
                     if self._length < 500: self._length += 1
                 elif opt == "shorter":
@@ -438,6 +474,7 @@ class Line:
                     self._doMove = True
                 elif opt == "newLine":
                     lines.append(Line())
+            
 
 
 
@@ -461,7 +498,7 @@ def stopScript():
 
 def main():
     global prjVersion, windowSize, lines, logfile
-    prjVersion = "2.0"
+    prjVersion = "2.3"
 
     runsys("")                      # Idk the purpose of this but it's needed in Windows to display proper VT100 sequences... (Windows dumb)
     windowSize = getWindowSize()
@@ -489,12 +526,13 @@ def main():
             sleep(args.speed)
             getSizeCounter += 1
 
+
     except KeyboardInterrupt:
         stopScript()
     
     except Exception as error:
         stopScript()
-        showMsg(error=f"Unhandled exception while running the script:\n\t'{error}'")
+        showMsg(error=f"Unhandled exception while running the script:\n\t'{error}'", type="Runtime")
 
 
 

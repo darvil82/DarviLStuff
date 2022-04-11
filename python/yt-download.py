b645d488fde5865757f88e45a90ad380d1f770df
DarviL82 <davidlosantos89@gmail.com>
2022-04-11 17:33:41 +0000

    update ytdownload to allow some options

--- python/yt-download.py
+++ python/yt-download.py
@@ -2,7 +2,9 @@ import requests
 import youtube_dl
 import os
 import eyed3
+import re
 
+from dataclasses import dataclass
 from PIL import Image
 
 
@@ -13,77 +15,118 @@ THUMB_CROP_SIZE = 280
 def print_center(text):
 	print(text.center(os.get_terminal_size()[0], "—"))
 
-if not os.path.isfile("songlist.txt"):
-	with open("songlist.txt", "w"):
-		pass
-	exit(0)
-
-os.makedirs("./songs", exist_ok=True)
-
-if os.path.isfile("./temp.webm.part"):
-	os.remove("./temp.webm.part")
+@dataclass
+class Options:
+	no_crop: bool
+	original_title: bool
 
 
 
-with open("songlist.txt", "r") as f:
-	songs = set(v for x in f.readlines() if (v := x.strip()) != "")
+def download_song(url: str, options: Options):
+	with youtube_dl.YoutubeDL({
+		"format": "bestaudio/best",
+		"postprocessors": [{
+			"key": "FFmpegExtractAudio",
+			"preferredcodec": "mp3",
+			"preferredquality": "192",
+		}],
+		"outtmpl": "./temp.%(ext)s",
+	}) as ydl:
+		song_data = get_song_data(url)
 
+		artist, track = (
+			song_data.get("artist", song_data["channel"]),
+			song_data["title"] if options.original_title else clean_title(song_data["title"])
+		)
 
-failed_songs = []
-
-
-with youtube_dl.YoutubeDL({
-	"format": "bestaudio/best",
-	"postprocessors": [{
-		"key": "FFmpegExtractAudio",
-		"preferredcodec": "mp3",
-		"preferredquality": "192",
-	}],
-	"outtmpl": "./temp.%(ext)s",
-}) as ydl:
-	for song in songs:
-		try:
-			song_data = ydl.extract_info(song, False)
-		except Exception:
-			failed_songs.append(song)
-			continue
-
-		artist, track = song_data["artist"], song_data["title"]
 		title = f"{artist} - {track}.mp3"
 
 		print_center(f"{artist} - {track}")
 
-		if os.path.isfile(f"./songs/{title}"):
-			print(f"Already exists! Skipping...")
-			continue
+		if os.path.isfile(f"./songs/{title.replace('/', '|')}"):
+			print(f"Already exists!")
+			return
 
 		# download image
 		with open("./temp.jpg", "wb") as f:
 			f.write(requests.get(song_data["thumbnails"][-1]["url"]).content)
 
-		# crop image
-		img = Image.open("./temp.jpg")
-		img = img.crop((THUMB_CROP_SIZE, 0, img.width - THUMB_CROP_SIZE, img.height))
-		img.save("./temp.jpg")
+		if not options.no_crop:
+			# crop image
+			img = Image.open("./temp.jpg")
+			img = img.crop((THUMB_CROP_SIZE, 0, img.width - THUMB_CROP_SIZE, img.height))
+			img.save("./temp.jpg")
 
-		ydl.download([song])
+		ydl.download([url])
 
 		# add metadata
 		audio_file = eyed3.load("./temp.mp3")
 		audio_file.initTag()
 		audio_file.tag.images.set(3, open("./temp.jpg", "rb").read(), "image/jpeg")
 		audio_file.tag.artist = artist
-		audio_file.tag.album = song_data["album"]
+		audio_file.tag.album = song_data.get("album")
 		audio_file.tag.title = track
 		audio_file.tag.save()
 
 		# move file
-		os.rename("./temp.mp3", f"./songs/{title}")
+		os.rename("./temp.mp3", f"./songs/{title.replace('/', '|')}")
 
 
-os.remove("./temp.jpg")
+def get_options_from_strs(options: list[str]) -> Options:
+	return Options(
+		no_crop = "no-crop" in options,
+		original_title = "original-title" in options
+	)
+
+
+def get_song_data(url: str) -> dict | bool:
+	with youtube_dl.YoutubeDL({
+		"format": "bestaudio/best",
+		"outtmpl": "./temp.%(ext)s",
+	}) as ydl:
+		try:
+			song_data = ydl.extract_info(url, False)
+		except Exception:
+			print("Error downloading song")
+			return False
+
+		return song_data
+
+
+def clean_title(title: str) -> str:
+	return re.sub(r"\([^)]*\)", "", title.split("-")[-1]).strip()
+
+
+def parse_entries(entry_list: list[str]):
+	for entry in entry_list:
+		if entry.startswith("#"):
+			continue
+
+		url, *options = entry.split()
+		download_song(url, get_options_from_strs(options))
 
 
 
-if failed_songs:
-	print("Failed:" + "\n\t".join(failed_songs))
+
+def main():
+	if not os.path.isfile("songlist.txt"):
+		with open("songlist.txt", "w"):
+			pass
+		exit(0)
+
+	os.makedirs("./songs", exist_ok=True)
+
+	if os.path.isfile("./temp.webm.part"):
+		os.remove("./temp.webm.part")
+
+	with open("songlist.txt", "r") as f:
+		songs = set(v for x in f.readlines() if (v := x.strip()) != "")
+		if failed := parse_entries(songs):
+			print("Failed:\n\t" + "\n\t".join(failed))
+
+	if os.path.isfile("./temp.jpg"):
+		os.remove("./temp.jpg")
+
+
+if __name__ == "__main__":
+	main()

ee7e5a1b875490138fe18a86ad33f953ec5edc6f
David L <48654552+DarviL82@users.noreply.github.com>
2022-09-26 10:31:13 +0200

    made shit code less shit

--- web/cookieHandler/cookies.ts
+++ web/cookieHandler/cookies.ts
@@ -1,15 +1,15 @@
 // This are the options that can be used when setting a cookie
-interface CookieOptions {
-	path?: string,
-	domain?: string,
-	maxAge?: number,
-	expires?: Date,
-	secure?: boolean,
-	samesite?: string
+export interface CookieOptions {
+	path?: string;
+	domain?: string;
+	maxAge?: number;
+	expires?: Date;
+	secure?: boolean;
+	samesite?: string;
 }
 
 // This is the value that a cookie can have
-type CookieValue = string | number | boolean | Object
+export type CookieValue = string | number | boolean | Object;
 
 
 /**
@@ -31,10 +31,8 @@ function camelToKebab(string: string): string {
  */
 function encodeValue(value: CookieValue): string {
 	return encodeURIComponent(
-		typeof value === "object"
-			? JSON.stringify(value)
-			: value
-	)
+		typeof value === "object" ? JSON.stringify(value) : value
+	);
 }
 
 /**
@@ -43,53 +41,51 @@ function encodeValue(value: CookieValue): string {
  * @returns
  */
 function decodeValue(value: string): CookieValue {
-	value = decodeURIComponent(value)
+	value = decodeURIComponent(value);
 	try {
-		return JSON.parse(value)
+		return JSON.parse(value);
 	} catch (error) {
-		if (/^[+-]?([0-9]*[.])?[0-9]+$/g.test(value))
-			return parseFloat(value)
-		else if (value === "true" || value === "undefined")
-			return true
-		else if (value === "false")
-			return false
-		else
-			return value
+		if (/^[+-]?([0-9]*[.])?[0-9]+$/g.test(value)) return parseFloat(value);
+		else if (value === "true" || value === "undefined") return true;
+		else if (value === "false") return false;
+		else return value;
 	}
 }
 
 
-class Cookies {
+export class Cookies {
 	/**
 	 * Set a cookie to the document.
 	 * @param key The name of the key of the cookie
 	 * @param value The value of the cookie
 	 * @param options Options for the cookie
 	 */
-	private _setCookie(key: string, value: CookieValue, options?: CookieOptions) {
-		let cookies = [`${key.replaceAll("=", "")}=${encodeValue(value)}`]
+	private static _setCookie(
+		key: string,
+		value: CookieValue,
+		options?: CookieOptions
+	) {
+		let cookies = [`${key.trim().replace(/=/g, "")}=${encodeValue(value)}`];
 
 		if (options)
 			Object.entries(options).forEach(([key, value]) => {
 				const pushValue = (value: string | true) => {
-					cookies.push(camelToKebab(key) +
-						(value !== true
-							? `=${value}`
-							: "")
-					)
-				}
+					cookies.push(
+						camelToKebab(key) + (value !== true ? `=${value}` : "")
+					);
+				};
 
 				switch (key) {
 					case "expires":
-						pushValue(value.toUTCString())
+						pushValue(value.toUTCString());
 						break;
 					default:
-						pushValue(value)
+						pushValue(value);
 						break;
 				}
-			})
+			});
 
-		document.cookie = `${cookies.join("; ")};`
+		document.cookie = `${cookies.join("; ")};`;
 	}
 
 	/**
@@ -100,10 +96,13 @@ class Cookies {
 	 * @param object Object containing multiple keys and values (or one) to set
 	 * @param options Options for the cookies that will be set
 	 */
-	set(object: { [key: string]: CookieValue }, options?: CookieOptions) {
+	public static set(
+		object: { [key: string]: CookieValue },
+		options?: CookieOptions
+	) {
 		Object.entries(object).forEach(([key, value]) => {
-			this._setCookie(key, value, options)
-		})
+			Cookies._setCookie(key, value, options);
+		});
 	}
 
 	/**
@@ -112,45 +111,40 @@ class Cookies {
 	 * @param name Optional key name to get
 	 * @returns An object with all cookies
 	 */
-	get(name?: string): Object {
-		const endObj: Object = {}
+	public static get(name?: string): Object {
+		const endObj: Object = {};
 
-		document.cookie.split(";").forEach(cookie => {
-			const [key, value] = cookie.trim().split("=")
-			if (!key) return
-			endObj[key] = decodeValue(value)
-		})
+		document.cookie.split(";").forEach((cookie) => {
+			const [key, value] = cookie.trim().split("=");
+			if (!key) return;
+			endObj[key] = decodeValue(value);
+		});
 
 		if (name) {
-			if (endObj[name])
-				return endObj[name]
+			if (endObj[name]) return endObj[name];
 			else {
-				console.error(`Cookie '${name}' does not exist`)
-				return
+				return null;
 			}
 		}
 
-		return endObj
+		return endObj;
 	}
 
 	/**
 	 * Remove a cookie from the document.
 	 * @param names Name/s of the cookies to remove
 	 */
-	remove(...names: string[]) {
-		names.forEach(name => {
-			if (!this.get(name)) return
-			this._setCookie(name, "", { maxAge: 0 })
-		})
+	public static remove(...names: string[]) {
+		names.forEach((name) => {
+			if (!Cookies.get(name)) return;
+			Cookies._setCookie(name, "", { maxAge: 0 });
+		});
 	}
 
 	/**
 	 * Remove all cookies from the document.
 	 */
-	removeAll() {
-		this.remove(...Object.keys(this.get()))
+	public static removeAll() {
+		Cookies.remove(...Object.keys(Cookies.get()));
 	}
 }
-
-
-const cookies = new Cookies()

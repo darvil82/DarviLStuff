94e529b2470ec11da858d766855264a37d35fcb2
DarviL82 <davidlosantos89@gmail.com>
2022-10-10 17:10:06 +0200

    update shit java thing

--- java/messages-bus/src/messaging/MessagesBus.java
+++ java/messages-bus/src/messaging/MessagesBus.java
@@ -2,6 +2,7 @@ package messaging;
 
 import java.util.ArrayList;
 import java.util.function.Consumer;
+
 import utils.Pair;
 
 
@@ -10,18 +11,13 @@ public class MessagesBus<TMsgTypeEnum extends Enum<TMsgTypeEnum>> {
 
 	@SuppressWarnings("unchecked cast")
 	public <TMsg extends Message<TMsgTypeEnum>> void addListener(Consumer<TMsg> callback, TMsgTypeEnum type) {
-		this.listeners.add(new Pair<>((Consumer<Message<TMsgTypeEnum>>)callback, type)); // probably a bit hacky
+		this.listeners.add(new Pair<>((Consumer<Message<TMsgTypeEnum>>) callback, type)); // probably a bit hacky
 	}
-	
+
 	public void sendMessage(Message<TMsgTypeEnum> msg) {
-		for (var pair : this.listeners) {
-			// if the type is correct, call the callback.
-			if (msg.type == pair.second) {
-				pair.first.accept(msg);
-			}
-		}
+		this.listeners.stream().filter((l) -> msg.type == l.second).forEach((l) -> l.first.accept(msg));
 	}
-	
+
 	public void sendMessages(Message<TMsgTypeEnum>[] msgs) {
 		for (var msg : msgs) {
 			this.sendMessage(msg);
